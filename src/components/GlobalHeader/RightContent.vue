92af892b5ae2c8402e6a30c87756457169d81f24
pengcl <3559996@qq.com>
2020-12-10 12:27:58 +0800

    init

---
+++ src/components/GlobalHeader/RightContent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="wrpCls">
+    <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="prefixCls" />
+    <select-lang :class="prefixCls" />
+    <a-select default-value="广汇尊府" style="width: 120px">
+      <a-select-option value="广汇尊府">
+        广汇尊府
+      </a-select-option>
+    </a-select>
+  </div>
+</template>
+
+<script>
+import AvatarDropdown from './AvatarDropdown'
+import SelectLang from '@/components/SelectLang'
+
+export default {
+  name: 'RightContent',
+  components: {
+    AvatarDropdown,
+    SelectLang
+  },
+  props: {
+    prefixCls: {
+      type: String,
+      default: 'ant-pro-global-header-index-action'
+    },
+    isMobile: {
+      type: Boolean,
+      default: () => false
+    },
+    topMenu: {
+      type: Boolean,
+      required: true
+    },
+    theme: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      showMenu: true,
+      currentUser: {}
+    }
+  },
+  computed: {
+    wrpCls () {
+      return {
+        'ant-pro-global-header-index-right': true,
+        [`ant-pro-global-header-index-${(this.isMobile || !this.topMenu) ? 'light' : this.theme}`]: true
+      }
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.currentUser = {
+        name: 'Serati Ma'
+      }
+    }, 1500)
+  }
+}
+</script>
+<style lang="less" scoped>
+  /deep/ .ant-select {
+    margin-left: 10px;
+    .ant-select-selection {
+      background: transparent;
+      color: #fff;
+    }
+    .ant-select-arrow {
+      color: #fff;
+    }
+  }
+</style>
